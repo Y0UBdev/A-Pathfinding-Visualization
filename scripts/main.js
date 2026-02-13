f43fe47238261dca06718048b44571b41cd2aa07
Sirius <boukhanoucheayoubpro@gmail.com>
2026-02-13 22:13:43 +0100

    Finish

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 import { Grid } from "./models/grid.js";
-import { stopEvent, applyEvent, bindGridEvents, resetEvent } from "./events/events.js";
+import { stopEvent, applyEvent, bindGridEvents } from "./events/events.js";
 import { Strategy } from "./core/strategy.js";
 
 const grid = new Grid(40, 30, 20);
